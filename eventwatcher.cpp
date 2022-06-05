535d668946d889ced85a671365e247c7aa0a20a2
Zhan <35628232+ehznahz@users.noreply.github.com>
2022-06-05 13:16:54 +0800

    Implment eventFilter to make hovered and pressed effect.

---
+++ eventwatcher.cpp
@@ -0,0 +1,49 @@
+#include "eventwatcher.h"
+#include "QGraphicsEffect"
+#include "stylizedbutton.h"
+#include "QApplication"
+
+eventWatcher::eventWatcher(QObject * parent) :QObject(parent)
+{}
+
+bool eventWatcher::eventFilter(QObject * watched, QEvent * event)
+{
+    stylizedButton * button = qobject_cast<stylizedButton*>(watched);
+    if (!button) {
+        return false;
+    }
+    //release
+    if (event->type() == QEvent::MouseButtonRelease){
+        if(button->isCheckable()&&button->isChecked())return false;
+        if(button->pressedPix==nullptr)delete button->graphicsEffect();
+        else{
+            button->setIcon(*(button->origPix));
+        }
+    }
+
+    //hover
+    if(event->type()==QEvent::Enter){
+        if(button->isCheckable()&&button->isChecked())return false;
+        //qDebug()<<"Hover In";
+        if(button->hoverPix==nullptr){
+            QGraphicsDropShadowEffect* effect = new QGraphicsDropShadowEffect();
+            effect->setColor(QColor(150,150,150,100));
+            effect->setBlurRadius(12);
+            effect->setOffset(0,0);
+            button->setGraphicsEffect(effect);
+        }
+        else{
+            button->setIcon(*(button->hoverPix));
+        }
+    }
+
+    if (event->type() == QEvent::Leave){
+        if(button->isCheckable()&&button->isChecked())return false;
+        //qDebug()<<"Hover Leave";
+        if(button->hoverPix==nullptr)delete button->graphicsEffect();
+        else{
+            button->setIcon(*(button->origPix));
+        }
+    }
+    return false;
+}
---
+++ eventwatcher.h
@@ -0,0 +1,16 @@
+#ifndef EVENTWATCHER_H
+#define EVENTWATCHER_H
+
+#include <QObject>
+#include <QPushButton>
+#include <QEvent>
+
+class eventWatcher:public QObject
+{
+    Q_OBJECT
+public:
+    eventWatcher(QObject * parent = Q_NULLPTR);
+    virtual bool eventFilter(QObject * watched, QEvent * event) Q_DECL_OVERRIDE;
+};
+
+#endif // EVENTWATCHER_H
