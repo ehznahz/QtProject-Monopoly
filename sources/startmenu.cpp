6ee841ed5981af06d209cedcfa7e45f8b16ff7d0
Zhan <35628232+ehznahz@users.noreply.github.com>
2022-06-30 18:58:00 +0800

    Finish.

--- sources/startmenu.cpp
+++ sources/startmenu.cpp
@@ -19,7 +19,43 @@ startMenu::startMenu(QWidget *parent): QWidget{parent}{
     infoBtn->setParent(this);
     infoBtn->move(this->width()*0.985-infoBtn->width(),this->height()*0.98-infoBtn->height());
     connect(infoBtn,&stylizedButton::clicked,[=](){
-        qDebug()<<"点击了info";
+        QWidget* pop = new QWidget();
+        pop->setObjectName("infoWidget");
+        pop->setStyleSheet("QWidget#infoWidget{border-image:url(:/resources/image/background-noIcon.png) 0 0 0 0 stretch stretch}"
+                           "QLabel{color:white;}");
+        pop->setParent(this->parentWidget());
+        pop->setWindowFlags(Qt::CustomizeWindowHint|Qt::FramelessWindowHint);
+        pop->setFixedSize(1600,900);
+        pop->move(0,0);
+        stylizedButton* close = new stylizedButton( 40, 40,":/resources/image/icons/close.png");
+        close->setParent(pop);
+        close->move(1500, 50);
+        close->show();
+        connect(close, &stylizedButton::pressed, this, [=]{
+            pop->close();
+        });
+        //文字
+        QLabel *text = new QLabel();
+        text->setParent(pop);
+        text->setText("关于");
+        text->setFont(QFont("Noto Sans SC",25,700));
+        text->setGeometry(this->width()*0.3,this->height()*0.2+50,500,40);
+        text->setAlignment(Qt::AlignLeft);
+
+        QLabel *content = new QLabel();
+        content->setParent(pop);
+        content->setText("该程序是北京大学2022年程序设计实习的大作业。\n"
+                         "贡献者:   @candidBoy @LeehWinCing @ehznahz\n\n"
+                         "该项目的完成离不开众多的开源软件和开放版权或免费内容:\n"
+                         "部分图标  来自  Icons 8;\n"
+                         "部分图标  来自  Emoji 项目;\n"
+                         "该程序由  Qt  驱动。\n\n"
+                         "该程序以  GNU LGPL v2.1  协议开放源码。");
+        content->setFont(QFont("Noto Sans SC",16,500));
+        content->setGeometry(this->width()*0.3,305,800,500);
+        content->setAlignment(Qt::AlignLeft);
+        text->show();
+        pop->show();
     });
 
     //退出游戏
@@ -57,14 +93,6 @@ startMenu::startMenu(QWidget *parent): QWidget{parent}{
         text->setStyleSheet("QLabel{color:white;}");
         text->show();
         pop->show();
-    });
-
-    //设置游戏
-    stylizedButton* settingBtn = new stylizedButton(40,40,":/resources/image/icons/setting.png");
-    settingBtn->setParent(this);
-    settingBtn->move(this->width()*0.95-settingBtn->width(),this->height()*0.08-exitBtn->height());
-    connect(settingBtn,&stylizedButton::clicked,[=](){
-        qDebug()<<"点击了setting";
     });
 }
 
